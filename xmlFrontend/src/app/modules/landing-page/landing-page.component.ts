25922f127fc7cae5599d92a2b4167b8b4108ef2b
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2023-03-29 21:26:30 +0200

    Add refresh page after delete flight

--- xmlFrontend/src/app/modules/landing-page/landing-page.component.ts
+++ xmlFrontend/src/app/modules/landing-page/landing-page.component.ts
@@ -5,60 +5,58 @@ import { FlightService } from '../service/flight.service';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+  allFlights: Array<Flight> = new Array();
+  isAdmin = false;
 
-  allFlights : Array<Flight> = new Array
-  isAdmin = false ;
-
-  constructor(private flightService: FlightService) { }
+  constructor(private flightService: FlightService) {}
 
   ngOnInit(): void {
     //  var loggedUserType = localStorage.getItem('loggedUserType')
     //  console.log(loggedUserType);
-     
+
     //     if(loggedUserType?.toString() === 'admin'){
     //       console.log(this.isAdmin);
-          
+
     //       this.isAdmin = true;
     //    }
-    
-    this.allUsers()
-    
+
+    this.allUsers();
   }
 
   allUsers() {
-    var loggedUserType = localStorage.getItem('loggedUserType')
-     console.log(loggedUserType);
-     
-        if(loggedUserType?.toString() === 'admin'){
-          console.log(this.isAdmin);
-          
-          this.isAdmin = true;
-       }
-    this.flightService.getAllFlights().subscribe(res => {
-      let resJSON = JSON.parse(res)
-      this.allFlights = resJSON
+    var loggedUserType = localStorage.getItem('loggedUserType');
+    console.log(loggedUserType);
+
+    if (loggedUserType?.toString() === 'admin') {
+      console.log(this.isAdmin);
+
+      this.isAdmin = true;
+    }
+    this.flightService.getAllFlights().subscribe((res) => {
+      let resJSON = JSON.parse(res);
+      this.allFlights = resJSON;
       this.allFlights.map((x) => {
-        const myDate = new Date(x.date)
-        // myDate.setHours(myDate.getHours() + 2) 
-        console.log(myDate)  
-        x.date = myDate.toLocaleString('en-US',{ timeZone: 'America/New_York' })
-        console.log(x.date)  
-
-      }
-      )
-      console.log(this.allFlights)             
-    })
+        const myDate = new Date(x.date);
+        // myDate.setHours(myDate.getHours() + 2)
+        console.log(myDate);
+        x.date = myDate.toLocaleString('en-US', {
+          timeZone: 'America/New_York',
+        });
+        console.log(x.date);
+      });
+      console.log(this.allFlights);
+    });
   }
 
-  deleteFlight(flight: Flight){
+  deleteFlight(flight: Flight) {
     console.log(flight.id);
-     this.flightService.deleteFlight(flight.id).subscribe(res =>{
-       console.log(res);
-       this.allUsers();
-     })
+    this.flightService.deleteFlight(flight.id).subscribe((res) => {
+      console.log(res);
+      this.allUsers();
+      window.location.href = '/';
+    });
   }
-
 }
